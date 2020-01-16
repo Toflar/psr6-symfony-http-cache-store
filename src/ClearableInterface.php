a53b70e00b620463b4d5559cc2478e51d0e4442d
Yanick Witschi <yanick.witschi@terminal42.ch>
2020-01-16 16:44:39 +0100

    Fixed missing interface for cache clear support

---
+++ src/ClearableInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of the toflar/psr6-symfony-http-cache-store package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @copyright  Yanick Witschi <yanick.witschi@terminal42.ch>
+ */
+
+namespace Toflar\Psr6HttpCacheStore;
+
+/**
+ * @author Yanick Witschi <yanick.witschi@terminal42.ch>
+ */
+interface ClearableInterface
+{
+    /**
+     * Clears the whole store.
+     */
+    public function clear();
+}
